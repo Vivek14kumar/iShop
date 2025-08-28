e425c8b9c265f93d688874254c5a865d0a1d8bcc
Vivek Kumar <vivek142000kumar@gamil.com>
2025-08-28 14:24:03 +0530

    prod

--- src/pages/admin/manageProduct.jsx
+++ src/pages/admin/manageProduct.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-//import API from "../../api/api";
+import API from "../../api/api";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { useNavigate } from "react-router-dom";
