517da467c5f1bc5a2a4d1a9fa1b2cbab4ec95880
Vivek Kumar <vivek142000kumar@gamil.com>
2025-08-29 11:05:53 +0530

    web

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,7 +1,8 @@
 import { io } from "socket.io-client";
 
 const SOCKET_URL = import.meta.env.VITE_API_URL || "https://ishop-2-f9qp.onrender.com";
+
 export const socket = io(SOCKET_URL, {
-  transports: ["polling"],
+  transports: ["websocket", "polling"], // try websocket first
   withCredentials: true,
 });
